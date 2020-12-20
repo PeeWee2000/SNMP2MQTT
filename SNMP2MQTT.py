b5dfa195906cd0424d6a712a45ab77549bcf801c
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2020-12-20 16:13:22 -0500

    Swtiched settings to use JSON and implemented functionality to send alerts for preconfigured devices

--- SNMP2MQTT.py
+++ SNMP2MQTT.py
@@ -1,61 +1,47 @@
+#System
+import sys
+import os
+import json
+
+#SNMP
 from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncore.dgram import udp
 from pysnmp.entity.rfc3413 import ntfrcv
 
+#MQTT
 import paho.mqtt.client as paho
 
-import yaml
-
-import sys
-import os
-
-
-
-
-
+#Classes currently unused -- put these in place in case the need/want to convert program to strongly typed arises
 class SNMPDevice:
-  def __init__(device, oid, age):
-    device.ticks = ticks
-    device.deviceoid = deviceoid
-    device.ip = ip
-    device.community = community
-    device.octet = octet    
-    device.payload = payload
+    def __init__(device, oid, age):
+        device.ticks = ticks
+        device.deviceoid = deviceoid
+        device.ip = ip
+        device.community = community
+        device.octet = octet    
+        device.payload = payload
 
+class ParentDevice:
+    def __init__(ParentDevice, ID, Name, Type, ChildDevices):
+        ParentDevice.ID = ID
+        ParentDevice.Name = Name
+        ParentDevice.Type = Type
+        ParentDevice.ChildDevices = ChildDevices
 
+class ChildDevice:
+    def __init__(ChildDevice, ID, Name, Type, MQTTName):
+        ChildDevice.ID = ID
+        ChildDevice.Name = Name
+        ChildDevice.Type = Type
+        ChildDevice.MQTTName = MQTTName
 
+ConfigPath = os.path.join(sys.path[0], 'SNMP2MQTT.json')
 
-
-
-
-#ConfigPath = os.path.join(sys.path[0], 'SNMP2MQTT.yaml')
-
-#with open(ConfigPath) as file:
-#    # The FullLoader parameter handles the conversion from YAML
-#    # scalar values to Python the dictionary format
-#    ConfigSettings = yaml.load(file, Loader=yaml.FullLoader)
-#    doc = yaml.load(file, Loader=yaml.FullLoader)
-
-#    sort_file = yaml.dump(doc, sort_keys=True)
-#    print(sort_file)
-
-
-#    print(ConfigSettings)
-
-
-
-#dict_file = [{'sports' : ['soccer', 'football', 'basketball', 'cricket', 'hockey', 'table tennis']},
-#{'countries' : ['Pakistan', 'USA', 'India', 'China', 'Germany', 'France', 'Spain']}]
-
-#with open(ConfigPath, 'w') as file:
-#    documents = yaml.dump(dict_file, file, sort_keys=True)
-
-
-
+with open(ConfigPath) as file:
+    DataMap = json.load(file)
 
 #MQTT Publishing Section
-
-broker="192.168.86.22"
+broker = DataMap.get('MQTTBrokerIP')
 port=1883
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
@@ -63,35 +49,85 @@ def on_publish(client,userdata,result):             #create function for callbac
 client1= paho.Client("control1")                           #create client object
 client1.on_publish = on_publish                          #assign function to callback
 
-
-
 #SNMP Trap Section
-
 snmpEngine = engine.SnmpEngine()
 
-TrapAgentAddress='192.168.86.229'; #Trap listerner address
-Port=162;  #trap listerner port
+TrapListenerAddress='192.168.86.229'; #Use the IP that the device sending packets would use not localhost
+TrapPort=162;
 
-print("Agent is listening SNMP Trap on "+TrapAgentAddress+" , Port : " +str(Port));
+print("Agent is listening SNMP Trap on "+TrapListenerAddress+" , Port : " +str(TrapPort));
 print('--------------------------------------------------------------------------');
 config.addTransport(
     snmpEngine,
     udp.domainName + (1,),
-    udp.UdpTransport().openServerMode((TrapAgentAddress, Port))
+    udp.UdpTransport().openServerMode((TrapListenerAddress, TrapPort))
 )
 
-#Configure community here
-config.addV1System(snmpEngine, 'Test', 'Test')
-config.addV1System(snmpEngine, 'my-area1', 'Waef')
+DeviceProfiles = list()
 
-def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
-          varBinds, cbCtx):
+#Create listener for each configured device
+DeviceConfigurations = DataMap['DeviceConfigurations']
+
+for DeviceConfiguration in DeviceConfigurations:
+    config.addV1System(snmpEngine, DeviceConfiguration['CommunityName'], DeviceConfiguration['CommunityName'])
+    print("Added SNMP listener for " + DeviceConfiguration['DeviceName'] + " using community " + DeviceConfiguration['CommunityName'])
+
+
+
+#Load settings for each configured device
+SNMPConfigs = list(DataMap['SNMPConfigurations'])
+
+
+#This function should be optimized with lambdas but works just fine as is -- would've done it myself but I'm no python expert
+def cbFun(snmpEngine, stateReference, contextEngineId, contextName, VariablePairs, cbContext):
     print("Received new Trap message");
-    for name, val in varBinds:        
-        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
-    client1.connect(broker,port)                             
-    client1.publish("Wewo","Trap Message Converted")
 
+    CurrentDevice = ""
+    IP = ""
+    ChildDeviceID = ""
+    ChildDeviceType = ""
+    ChildDeviceValue = ""
+    MQTTMessage = ""
+    Sequence = 0
+    
+    for NameObject, ValueObject in VariablePairs:
+        Name = NameObject.prettyPrint()
+        Value = ValueObject.prettyPrint()
+        print('%s = %s' % (Name, Value))
+
+        for SNMPConfig in SNMPConfigs:            
+            if (Name == SNMPConfig['IdentifierOID']) & (Value == SNMPConfig['IdentifierValue']):
+                CurrentDevice = SNMPConfig['DeviceName']
+                break
+                
+    for SNMPVariableBinding in list(SNMPConfig['SNMPVariableBindings']):
+        BindingName = SNMPVariableBinding.get('Name')
+
+        if BindingName == "IP" :
+            Sequence = SNMPVariableBinding.get('Sequence')
+            IP = VariablePairs[Sequence][1].prettyPrint() #Tuple 1 = Value
+
+        elif  BindingName == "ChildDeviceType" :
+            Sequence = SNMPVariableBinding.get('Sequence')
+            ChildDeviceTypeOctet = VariablePairs[Sequence][1].prettyPrint() #Tuple 1 = Value
+
+            for ChildDeviceTypePair in list(SNMPConfig['ChildDeviceTypes']) :
+                if ChildDeviceTypePair.get('Value') == ChildDeviceTypeOctet :
+                    ChildDeviceType = ChildDeviceTypePair.get('Type')
+                    break
+
+        elif  BindingName == "ChildDeviceID" :
+            Sequence = SNMPVariableBinding.get('Sequence')
+
+        elif  BindingName == "ChildDeviceValue" :
+            Sequence = SNMPVariableBinding.get('Sequence')
+            ChildDeviceID = VariablePairs[Sequence][0].prettyPrint() #Tuple 0 = Name
+            ChildDeviceValue = VariablePairs[Sequence][1].prettyPrint() #Tuple 1 = Value
+
+    #for DeviceConfig in DeviceConfigurations :
+
+    client1.connect(broker,port)                             
+    client1.publish("Wewo", CurrentDevice + " @ IP:" + IP + " " + ChildDeviceType + " " + ChildDeviceID + " = " + ChildDeviceValue)
 
 ntfrcv.NotificationReceiver(snmpEngine, cbFun)
 
@@ -103,3 +139,8 @@ try:
 except:
     snmpEngine.transportDispatcher.closeDispatcher()
     raise
+
+
+
+
+
